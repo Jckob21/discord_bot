7f1783174b84774609e59d294d80ef058a728cb2
Jacob <jckob21@gmail.com>
2022-01-17 15:57:15 +0000

    Established connection for the bot

---
+++ main.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+    console.log('CurveBan is alive!');
+});
+
+
+
+
+
+
+
+// last line of the file
+client.login('OTMyNjYyMDExMDYwNDIwNjY4.YeWPLw.aq8SHborla81YU-wSwr1iTO7HYc');
