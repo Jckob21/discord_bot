f36becec070e85cb1e743d436f1520f0236c8f26
Jacob <jckob21@gmail.com>
2022-01-18 16:56:25 +0000

    Added emperor command handling

--- main.js
+++ main.js
@@ -8,6 +8,23 @@ client.once('ready', () => {
 });
 
 
+const prefix = '!';
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    // remove the prefix and split the message into words
+    const args = message.content.slice(prefix.length).split(" ");
+
+    // get the first word of the message and toLowerCase
+    const command = args.shift().toLowerCase();
+
+
+    if(command === 'emperor') {
+        message.channel.send('The only true emperor of the server is Jcob21 fucking bastards.');
+    }
+});
+
 
 
 var json = require("./token.json");
